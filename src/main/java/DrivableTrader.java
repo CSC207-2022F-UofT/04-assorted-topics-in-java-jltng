8befdf476e61e303736156052b2b55d709d40a37
jltng <julie.x.tang@outlook.com>
2022-10-08 17:34:59 -0400

    TODOs completed.

--- src/main/java/DrivableTrader.java
+++ src/main/java/DrivableTrader.java
@@ -11,3 +11,30 @@
  */
 
 import java.util.List;
+
+class DrivableTrader extends Trader<Drivable> {
+    /**
+     * A subclass of Trader.
+     * Contains only Drivable objects.
+     *
+     * @param inventory Objects in this DrivableTrader's inventory
+     * @param wishlist  Objects in this DrivableTrader's wishlist
+     * @param money     The DrivableTrader's money
+     */
+    public DrivableTrader(List<Drivable> inventory, List<Drivable> wishlist, int money) {
+        super(inventory, wishlist, money);
+    }
+    public DrivableTrader(int money) {
+        super(money);
+    }
+    @Override
+    public int getSellingPrice(Drivable object) {
+        int price = super.getSellingPrice(object);
+
+        if (price == Tradable.MISSING_PRICE) {
+            return price;
+        } else {
+            return price + object.getMaxSpeed();
+        }
+    }
+}
